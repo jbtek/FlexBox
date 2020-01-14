7bcb24447eb8ecc0243cef0d74b4e9a068831f91
jkushwaha <jkushwaha@wdtablesystems.com>
2020-01-14 17:53:23 +0530

    added prototype patter in ES6

--- src/designpatterns/ES6/CreationalPatterns/PrototypePattern.js
+++ src/designpatterns/ES6/CreationalPatterns/PrototypePattern.js
@@ -1,3 +1,20 @@
 class PrototypePattern {
-    
+    constructor (id, x, y) {
+        this.id = id
+        this.move(x, y);
+    }
+    move (x, y) {
+        this.x = x
+        this.y = y
+    }
 }
+
+//above is equivalant to this in es5
+/*const PrototypePattern = function (id, x, y) {
+    this.id = id;
+    this.move(x, y);
+};
+PrototypePattern.prototype.move = function (x, y) {
+    this.x = x;
+    this.y = y;
+};*/
