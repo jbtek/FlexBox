01426d522da0e1c24877c19867eaa2028221e38a
jkushwaha <jkushwaha@wdtablesystems.com>
2020-01-20 10:28:54 +0530

    added js features

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {fancyLabel} from "./src/webcomponenets/FancyLabel.js";
-import {userCard} from "./src/UserCard/UserCard.js";
+import {fancyLabel} from "./src/jsfeatures/WebAPI/webcomponenets/FancyLabel.js";
+import {userCard} from "./src/jsfeatures/WebAPI/UserCard/UserCard.js";
 import {ConstructorPattern} from "./src/designpatterns/ES6/CreationalPatterns/ConstructorPattern.js";
 import {constructorPattern} from "./src/designpatterns/ES5/CreationalPatterns/ConstructorPattern.js";
 import {Warrior, Warrior1,Warrior2} from "./src/designpatterns/ES5/CreationalPatterns/PrototypePattern.js";
